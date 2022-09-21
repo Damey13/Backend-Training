acce73edbf5d8919f6152ee525a25a049d2e9da2
Damey13 <dameyabraham@gmail.com>
2022-09-21 10:18:47 +0530

    loop pgms

---
+++ src/com/orthofx/PyramidStar.java
@@ -0,0 +1,31 @@
+package com.orthofx;
+
+import java.util.Scanner;
+
+public class PyramidStar {
+
+	public static void main(String[] args) {
+		Scanner input=new Scanner(System.in);
+		System.out.println("Enter a number");
+		int num=input.nextInt();
+		
+		for (int i=0; i<num; i++)
+        {       
+            for (int j=num-i; j>1; j--)
+            {
+               
+                System.out.print(" ");
+            }         
+
+            for (int j=0; j<=i; j++ )
+            {
+               
+                System.out.print("* ");
+            }
+             
+            System.out.println();
+        }
+    }
+	}
+
+
---
+++ src/com/orthofx/PyramidStarWhiile.java
@@ -0,0 +1,33 @@
+package com.orthofx;
+
+import java.util.Scanner;
+
+public class PyramidStarWhiile {
+
+	public static void main(String[] args) {
+		Scanner input=new Scanner(System.in);
+		System.out.println("Enter a number");
+		int num=input.nextInt();
+		int i=0,j=0;
+		while (i<num)
+        {    
+		 j=num-i;
+            while (j>1)
+            {
+                System.out.print(" ");
+                j--;
+            }         
+             j=0; 
+            while (j<=i)
+            {
+                System.out.print("* ");
+                j++;
+            }
+             
+            System.out.println();
+            i=i+1;
+        }
+    }
+	}
+
+
